a2af91eab6de8501ac2a8265a32c77e30c7121f7
Nguyễn Văn Khải <khai20041802@gmail.com>
2025-03-04 12:18:47 +0700

    Delete doan directory

--- doan/main.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from gui import create_interface
-from callbacks import scan_url_callback  
-
-root, selected_function = create_interface(scan_url_callback)
-
-root.mainloop()  # Chạy giao diện
--- doan/process.py
+++
@@ -1,5 +0,0 @@
-import requests
-import threading
-def fuzzing(url_target):
-    print(f"[+] Processing fuzzing: {url_target}")
-    
